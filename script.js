d357a4ad3d23a1f4e88208beed1d1e915d49f379
Nelutu Nemes <nelutunemes10@gmail.com>
2025-02-13 21:52:19 +0200

    second commit

--- script.js
+++ script.js
@@ -5,21 +5,20 @@ let log = (message) => {
         console.log(message);
     }
 }
-log(`Debuging is activ !`);
+log(`Debug is active !`);
 
 //get reference to the DOM elements
 let display = document.getElementById("display");
 let buttons = document.querySelectorAll(".btn");
 
-function startDeviceSimulator() {
+(()=> {
     display.classList.add("smallFont")
     display.textContent = "... initalizing device"
     setTimeout(() => {
         display.classList.remove("smallFont")
         display.textContent = "0"
     },1000)
-}
-startDeviceSimulator();
+})();
 
 log(`Find:${buttons.length} buttons.`);
 log(`All buttons:${Array.from(buttons).map((button)=>button.getAttribute("data-value"))}`)
@@ -36,12 +35,35 @@ let operations = {
     "+": (a, b) => a + b,
     "-":(a,b)=> a - b,
     "*":(a,b)=> a * b,
-    "/":(a,b)=>(b>0) ? a / b : "Error"
+    "/":(a,b)=>(b!== 0) ? a / b : "Error"
 }
 
 //function to set first and second operand
+function setOperand() {
+    if (firstOperand === null && currentInput === "") return;//prevent set an empty operand
+    if (firstOperand === null) {
+        firstOperand = parseFloat(currentInput);
+        log(`First operand is: ${firstOperand}`);
+    } else if (operator !== null && currentInput !== "") {
+        secondOperand = parseFloat(currentInput);
+        log(`Second operand is: ${secondOperand}`);
+    }
+    currentInput = "";
+    updateUi();
+}
 
 //function to set operator
+function setOperator(value) {
+    if (firstOperand === null && currentInput === "") return;
+    if (currentInput) setOperand();
+
+    if (firstOperand !== null && currentInput !== null && secondOperand !== null) {
+        calculate()
+    }
+    operator = value;
+    log(`Operator set: ${operator}`);
+    updateUi();
+}
 
 //function to update UI
 function updateUi() {
@@ -52,8 +74,48 @@ function updateUi() {
 }
 
 //main function for calculation
-
+function calculate(){
+    if (firstOperand !== null && operator !== null && secondOperand !== null)  {
+        let result = operations[operator](firstOperand, secondOperand);
+        firstOperand = result;
+        secondOperand = null;
+        currentInput = "";
+        operator = null;
+        log(`Result : ${firstOperand}`);
+        updateUi();
+}
+}
 //aditional function
+function resetCalculator() {
+    firstOperand = null;
+    secondOperand = null;
+    operator = null;
+    currentInput = "";
+    display.textContent = "0";
+    log(`Reset calculator !`);
+}
+
+function deleteLastChar() {
+    if (currentInput !== "") {
+        //if there is a current input, delete last digit
+        currentInput = currentInput.slice(0, -1);
+        if (currentInput === "") {
+            resetCalculator();
+            return;
+        }
+    } else if (firstOperand !== null && operator === null) {
+        currentInput = firstOperand.toString();
+        firstOperand = null;
+        if (currentInput.length > 1) {
+            currentInput = currentInput.slice(0, -1);//remove last digit of displayed result
+        } else {
+            resetCalculator();
+            return;
+    }
+    }
+    updateUi();
+}
+
 
 function handleNumberInput(value) {
     if (value === "." && currentInput.includes(".")) return//prevent double decimals
@@ -64,11 +126,11 @@ function handleNumberInput(value) {
     updateUi();
 }
 
-//add event listener to buttons
+// Add a click event listener to each button and handle the user's input
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        let value = button.getAttribute("data-value");
-        log(`Button presed is: ${value}`)
+        let value = button.getAttribute("data-value") ||"";
+        log(`Button pressed is: ${value}`)
 
 
         switch (true) {
@@ -76,15 +138,22 @@ buttons.forEach((button) => {
             case value === ".":
                 handleNumberInput(value);
                 break;
-            
-            
-            
-            
-            
-            
+            case value in operations:
+                setOperator(value);
+                break;
+            case value === "=":
+                setOperand();
+                calculate();
+                break;
+            case value === "C":
+                resetCalculator();
+                break;
+            case value === "DEL":
+                deleteLastChar();
+                break;
             
             default:
-                log(`Unkdown button value${value}`);
+                log(`Unknown button value${value}`);
                 break;
         }
     });
